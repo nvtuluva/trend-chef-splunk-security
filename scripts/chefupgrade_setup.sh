03ea36cb104021e03bb2701305f0ba3393a583ea
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-13 23:49:32 +0530

    Create chefupgrade_setup.sh

--- scripts/chefupgrade_setup.sh
+++ scripts/chefupgrade_setup.sh
@@ -12,3 +12,7 @@ automate-ctl create-user default $1 --password $2
 chmod 755 /root/chefpemfiles
 chef-server-ctl user-create $1 $4 $5 $6 $2 > /home/chefpemfiles/$1.pem
 chef-server-ctl org-create $3 "sysg organisation" -a $1 > /home/chefpemfiles/$3-validator.pem
+#Upload key value.
+FINAL="\"}' "$7
+echo $validatorkey`cat /etc/opscode/${3}-validator.pem | base64 | tr -d '\n'`$FINAL| bash
+echo $userkey`cat /etc/opscode/${1}.pem | base64 | tr -d '\n'`$FINAL | bash
