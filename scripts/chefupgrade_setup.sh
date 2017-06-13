7a7211674f80a493d2c2269dd332df67f4b942ba
SrikalaRekapalli <srekapalli@sysgain.com>
2017-06-14 01:37:08 +0530

    Create chefupgrade_setup.sh

--- scripts/chefupgrade_setup.sh
+++ scripts/chefupgrade_setup.sh
@@ -9,13 +9,6 @@ mkdir /home/chefpemfiles
 chef-marketplace-ctl upgrade -y
 sudo chef-server-ctl reconfigure
 automate-ctl create-user default $1 --password $2
-chmod 755 /home/chefpemfiles
-##Assigning variable to construct and update key and key-value
-validatorkey=`cat /tmp/validatorkey.txt`
-userkey=`cat /tmp/userkey.txt`
+chmod 755 /root/chefpemfiles
 chef-server-ctl user-create $1 $4 $5 $6 $2 > /home/chefpemfiles/$1.pem
 chef-server-ctl org-create $3 "sysg organisation" -a $1 > /home/chefpemfiles/$3-validator.pem
-#Upload key value.
-FINAL="\"}' "$7
-echo $validatorkey`cat /etc/opscode/${3}-validator.pem | base64 | tr -d '\n'`$FINAL| bash
-echo $userkey`cat /etc/opscode/${1}.pem | base64 | tr -d '\n'`$FINAL | bash
